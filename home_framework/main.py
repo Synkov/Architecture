6ea411cca7695899050efe349d26a2974eb6d72b
aleksei <synkovaleksei@yandex.ru>
2022-08-13 17:42:12 +0300

    Lesson_2. Added framework_requests

--- home_framework/main.py
+++ home_framework/main.py
@@ -1,3 +1,6 @@
+import quopri
+from framework_requests import GetRequests, PostRequests
+
 class PageNotFound404:
     def __call__(sell):
         return '404 WHAT', '404 Page Not Found'
@@ -13,6 +16,20 @@ class Framework:
         if not path.endswith('/'):
             path = f'{path}/'
 
+        request = {}
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            request['data'] = data
+            print(f'Нам пришел POST-запрос: {Framework.decode_value(data)}')
+
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['requests_params'] = request_params
+            print(f'Нам пришли GET-параметры: {request_params}')
+
         if path in self.routes_lst:
             view = self.routes_lst[path]
         else:
@@ -20,3 +37,12 @@ class Framework:
         code, body = view()
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace('+', ' '), 'UTF-8')
+            val_decode_str = quopri.decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
